8afb47156b24e6bb1daede7e51850501276bb211
Tatevik <tshavarshevna@gmail.com>
2023-02-05 12:38:35 +0300

    task3

---
+++ sem0401/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int B = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+for(int i = 0; i < B; i++)
+{
+    count *= A;
+}
+Console.Write(count);
