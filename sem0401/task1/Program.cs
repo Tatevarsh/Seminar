cc09645f8beadaa8b695cd9970986fd6186b1743
Tatevik <tshavarshevna@gmail.com>
2023-02-05 10:39:11 +0300

    task 1

---
+++ sem0401/task1/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Найти сумму чисел от 1 до A */
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int sum = 0;
+for (int i = 0; i <= num; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine(sum);
